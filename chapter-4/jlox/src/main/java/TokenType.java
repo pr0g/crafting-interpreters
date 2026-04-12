dde5e39cf7030ab24472b1565cb24d26e6d37deb
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2026-04-12 21:38:44 +0100

    chapter 4 (initially) complete

--- chapter-4/jlox/src/main/java/TokenType.java
+++ chapter-4/jlox/src/main/java/TokenType.java
@@ -1,6 +1,6 @@
 package com.craftinginterpreters.lox;
 
-enum TokeType {
+enum TokenType {
   // single character tokens
   LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
   // one or two character tokens
