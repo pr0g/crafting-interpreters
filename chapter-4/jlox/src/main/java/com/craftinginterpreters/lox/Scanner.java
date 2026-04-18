944359751677b5090d193cebfafda7010107eed4
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2026-04-18 14:22:47 +0100

    ensure we handle end of multi-line comment

--- chapter-4/jlox/src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ chapter-4/jlox/src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -60,7 +60,12 @@ class Scanner {
       case '-': addToken(MINUS); break;
       case '+': addToken(PLUS); break;
       case ';': addToken(SEMICOLON); break;
-      case '*': addToken(STAR); break;
+      case '*': 
+        if (match('/')) {
+          break;
+        } else {
+          addToken(STAR); break;
+        }
       case '!': 
         addToken(match('=') ? BANG_EQUAL : BANG);
         break;
