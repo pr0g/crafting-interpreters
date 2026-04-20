c331a4842a04e72b5aede7504a77f8e9da906063
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2026-04-20 22:20:13 +0100

    add simple test program to validate printer

--- jlox/src/main/java/com/craftinginterpreters/lox/AstPrinter.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/AstPrinter.java
@@ -10,12 +10,12 @@ class AstPrinter implements Expr.Visitor<String> {
     return parenthesize(
         expr.operator.lexeme, expr.left, expr.right);
   }
-  
+
   @Override
   public String visitGroupingExpr(Expr.Grouping expr) {
     return parenthesize("group", expr.expression);
   }
-  
+
   @Override
   public String visitLiteralExpr(Expr.Literal expr) {
     if (expr.value == null) {
@@ -23,15 +23,15 @@ class AstPrinter implements Expr.Visitor<String> {
     }
     return expr.value.toString();
   }
-  
+
   @Override
   public String visitUnaryExpr(Expr.Unary expr) {
     return parenthesize(expr.operator.lexeme, expr.right);
   }
-  
+
   private String parenthesize(String name, Expr... exprs) {
     StringBuilder builder = new StringBuilder();
-    
+
     builder.append("(").append(name);
     for (Expr expr : exprs) {
       builder.append(" ");
@@ -39,5 +39,16 @@ class AstPrinter implements Expr.Visitor<String> {
     }
     builder.append(")");
     return builder.toString();
+  }
+
+  public static void main(String[] args) {
+    Expr expression = new Expr.Binary(
+      new Expr.Unary(
+        new Token(TokenType.MINUS, "-", null, 1), 
+        new Expr.Literal((123))),
+      new Token(TokenType.STAR, "*", null, 1),
+      new Expr.Grouping(
+        new Expr.Literal(45.67)));
+      System.out.println(new AstPrinter().print(expression));
   }
 }
