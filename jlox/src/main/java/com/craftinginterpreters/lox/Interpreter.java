c3406adf76f901ba25f541d7efa4ca5e2792a21b
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2026-06-20 14:30:06 +0100

    add support for calling functions

--- jlox/src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -152,6 +152,13 @@ class Interpreter implements Expr.Visitor<Object>,
     evaluate(stmt.expression);
     return null;
   }
+  
+  @Override
+  public Void visitFunctionStmt(Stmt.Function stmt) {
+    LoxFunction function = new LoxFunction(stmt);
+    environment.define(stmt.name.lexeme, function);
+    return null;
+  }
 
   @Override
   public Void visitIfStmt(Stmt.If stmt) {
