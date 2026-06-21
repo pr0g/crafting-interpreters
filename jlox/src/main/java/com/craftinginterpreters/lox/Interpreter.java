fd234aea6bd3c402f43a6b3c34568a8fa127e9a1
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2026-06-21 14:38:02 +0100

    fix closure bugs - add resolver (initial)

--- jlox/src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,7 +1,9 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Interpreter implements Expr.Visitor<Object>,
     Stmt.Visitor<Void> {
@@ -10,6 +12,7 @@ class Interpreter implements Expr.Visitor<Object>,
 
   final Environment globals = new Environment();
   private Environment environment = globals;
+  private final Map<Expr, Integer> locals = new HashMap<>();
   
   Interpreter() {
     globals.define("clock", new LoxCallable() {
@@ -127,6 +130,10 @@ class Interpreter implements Expr.Visitor<Object>,
 
   private void execute(Stmt stmt) {
     stmt.accept(this);
+  }
+  
+  void resolve(Expr expr, int depth) {
+    locals.put(expr, depth);
   }
 
   void executeBlock(List<Stmt> statements, Environment environment) {
