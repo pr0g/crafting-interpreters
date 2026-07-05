66906d13c7b9efedbb6d8d6e5f9da755fddaab1c
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2026-07-05 21:14:56 +0100

    ensure this can only be used in valid contexts

--- jlox/src/main/java/com/craftinginterpreters/lox/Resolver.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/Resolver.java
@@ -9,6 +9,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private final Interpreter interpreter;
   private final Stack<Map<String, Boolean>> scopes = new Stack<>();
   private FunctionType currentFunction = FunctionType.NONE;
+  private ClassType currentClass = ClassType.NONE;
 
   Resolver(Interpreter interpreter) {
     this.interpreter = interpreter;
@@ -18,6 +19,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     NONE,
     METHOD,
     FUNCTION
+  }
+
+  private enum ClassType {
+    NONE,
+    CLASS
   }
 
   void resolve(List<Stmt> statements) {
@@ -71,6 +77,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitClassStmt(Stmt.Class stmt) {
+    ClassType enclosingClass = currentClass;
+    currentClass = ClassType.CLASS;
     declare(stmt.name);
     beginScope();
     scopes.peek().put("this", true);
@@ -80,6 +88,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
       resolveFunction(method, declaration);
     }
     endScope();
+    currentClass = enclosingClass;
     return null;
   }
 
@@ -214,6 +223,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitThisExpr(Expr.This expr) {
+    if (currentClass == ClassType.NONE) {
+      Lox.error(expr.keyword,
+          "Can't use 'this' outside of a class.");
+      return null;
+    }
     resolveLocal(expr, expr.keyword);
     return null;
   }
