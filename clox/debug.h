d40e5a556b0c659da0262b4de3f7f0ce272c582e
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2026-08-31 22:31:20 +0100

    add debug output for chunks

---
+++ clox/debug.h
@@ -0,0 +1,10 @@
+#ifndef CLOX_DEBUG_H
+#define CLOX_DEBUG_H
+
+// typedef struct Chunk;
+#include "chunk.h"
+
+void disassemble_chunk(Chunk* chunk, const char* name);
+int disassemble_instruction(Chunk* chunk, int offset);
+
+#endif // CLOX_DEBUG_H
